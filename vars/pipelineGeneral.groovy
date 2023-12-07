d0220438a6eb5c62f2aeedc705fe3c4db4167036
edansanmar <esmartinezea2@gmail.com>
2023-12-07 15:42:34 -0500

    Se utilizo una función en donde se adiciono todo el Pipeline

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,54 +1,55 @@
-pipeline {
-    agent any
+def pipelineGeneral() {
+    pipeline {
+        agent any
 
-    stages {
-        stage('Checkout') {
-            steps {
-                git url: 'https://github.com/edansanmar/termometro.git'
-            }
-        }
-
-        stage('Build Application') {
-            steps {
-                sh 'mvn clean package'
-            }
-        }
-
-     stage('Test') {
-            steps {
-                sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
-            }
-        }
-        stage('Package'){
-            steps{
-                sh 'mvn package'
-            }
-            post {
-                always{
-                    junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
-                }
-                success{
-                    
-                    archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
-                    
+        stages {
+            stage('Checkout') {
+                steps {
+                    git url: 'https://github.com/edansanmar/termometro.git'
                 }
             }
-        }
-        stage('SonarQube analysis') {
-            environment {
-                scannerHome = tool 'SonarqubeScanner'
-            }
-            steps {
-                withSonarQubeEnv('ServerSonarqube') {
-                    sh "${scannerHome}/bin/sonar-scanner \
-                       -Dsonar.projectKey=analisisTermometro \
-                        -Dsonar.projectName=analisisTermometro \
-                       -Dsonar.sources=src/main/java \
-                       -Dsonar.java.binaries=target/classes \
-                       -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+
+            stage('Build Application') {
+                steps {
+                    sh 'mvn clean package'
                 }
             }
-        } 
-        
+
+            stage('Test') {
+                steps {
+                    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    sh 'mvn package'
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
+                    }
+                    success {
+                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
+                    }
+                }
+            }
+
+            stage('SonarQube analysis') {
+                environment {
+                    scannerHome = tool 'SonarqubeScanner'
+                }
+                steps {
+                    withSonarQubeEnv('ServerSonarqube') {
+                        sh "${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=analisisTermometro \
+                            -Dsonar.projectName=analisisTermometro \
+                            -Dsonar.sources=src/main/java \
+                            -Dsonar.java.binaries=target/classes \
+                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+                    }
+                }
+            }
+        }
     }
 }
