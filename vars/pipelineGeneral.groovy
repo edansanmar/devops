ee28d7db65e5ea8b9432e587f7e15d05eea2e166
edansanmar <esmartinezea2@gmail.com>
2023-12-11 13:42:21 -0500

    Cambio del link de repositorio, a la varaible que se usa para obtener el repositorio, para verificar el funcionamiento

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -9,7 +9,7 @@ pipeline {
         stages {
             stage('Checkout') {
                 steps {
-                    git url: 'https://github.com/edansanmar/termometro.git'
+                    git url: scmUrl
                 }
             }
 
