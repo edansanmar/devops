2d83a8fd7325c0dc13a78190fc4a1dea00bdb00c
edansanmar <esmartinezea2@gmail.com>
2023-12-11 17:13:44 -0500

    Se modifican los archivo pipelineGeneral y adiciona y creal el archivo de creación del artefacto

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -3,58 +3,10 @@ def call (Map params) {
     def scmUrl = params.scmUrl
 
     echo "Deploying backend wiht SCM URL: ${scmUrl}"
-pipeline {
-        agent any
-
-        stages {
-            stage('Checkout') {
-                steps {
-                    git url: scmUrl
-                }
-            }
-
-            stage('Build Application') {
-                steps {
-                    sh 'mvn clean package'
-                }
-            }
-
-            stage('Test') {
-                steps {
-                    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
-                }
-            }
-
-            stage('Package') {
-                steps {
-                    sh 'mvn package'
-                }
-                post {
-                    always {
-                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
-                    }
-                    success {
-                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
-                    }
-                }
-            }
-
-            stage('SonarQube analysis') {
-                environment {
-                    scannerHome = tool 'SonarqubeScanner'
-                }
-                steps {
-                    withSonarQubeEnv('ServerSonarqube') {
-                        sh "${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=analisisTermometro \
-                            -Dsonar.projectName=analisisTermometro \
-                            -Dsonar.sources=src/main/java \
-                            -Dsonar.java.binaries=target/classes \
-                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
-                    }
-                }
-            }
-        }
+    
+    //Importa scripts externos
+    script {
+        load 
     }
   
 }
