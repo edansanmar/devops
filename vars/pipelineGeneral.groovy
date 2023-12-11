8955745c340edfdae45fca154951096208135905
edansanmar <esmartinezea2@gmail.com>
2023-12-11 11:20:32 -0500

    Prueba del funcionamiento del archivo pipeline, de una nueva forma

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,54 +1,16 @@
-    pipeline {
-        agent any
+//File: pipelineGeneral.groovvy
+def call (Map params) {
+    def scmUrl = params.scmUrl
 
-        stages {
-            stage('Checkout') {
-                steps {
-                    git url: 'https://github.com/edansanmar/termometro.git'
-                }
-            }
+    echo "Deploying backend wiht SCM URL: ${scmUrl}"
+    
+    // Clonar el repositorio
+    echo "Clonación de repositorio"
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: scmUrl]]])
 
-            stage('Build Application') {
-                steps {
-                    sh 'mvn clean package'
-                }
-            }
+    // Construir la aplicación con Maven
+    echo "Contruir la aplicación con Maven"
+    sh 'mvn clean package'
 
-            stage('Test') {
-                steps {
-                    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
-                }
-            }
 
-            stage('Package') {
-                steps {
-                    sh 'mvn package'
-                }
-                post {
-                    always {
-                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
-                    }
-                    success {
-                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
-                    }
-                }
-            }
-
-            stage('SonarQube analysis') {
-                environment {
-                    scannerHome = tool 'SonarqubeScanner'
-                }
-                steps {
-                    withSonarQubeEnv('ServerSonarqube') {
-                        sh "${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=analisisTermometro \
-                            -Dsonar.projectName=analisisTermometro \
-                            -Dsonar.sources=src/main/java \
-                            -Dsonar.java.binaries=target/classes \
-                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
-                    }
-                }
-            }
-        }
-    }
-
+}
