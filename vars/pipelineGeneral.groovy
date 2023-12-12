daec267901fb2af6304f805ac32defddbd598cdd
edansanmar <esmartinezea2@gmail.com>
2023-12-12 13:24:14 -0500

    Nueva prueba

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -9,6 +9,11 @@ def call(Map params) {
     pipeline {
         agent any
         stages {
+            stage('Checkout') {
+        steps {
+            git url: scmUrl
+        }
+    }
             stage('Step 1') {
                 steps {
                     // Llama a la función de la biblioteca compartida pasando scmUrl como argumento
--- vars/prueba.groovy
+++ vars/prueba.groovy
@@ -3,11 +3,7 @@
 def artefacto(scmUrl) {
     echo "Primera parte de la ejecución del Pipeline"
     // Clonación del repositorio 
-    stage('Checkout') {
-        steps {
-            git url: scmUrl
-        }
-    }
+    
     // Etapa de construcción de la aplicación
     stage('Build Application') {
         steps {
