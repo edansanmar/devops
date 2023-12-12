98b305d60053bf4e92ea1ddbb347ba11aa376427
edansanmar <esmartinezea2@gmail.com>
2023-12-12 17:45:58 -0500

    nuevo cambio

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -5,7 +5,7 @@ def call(Map params) {
     echo "Deploying backend with SCM URL: ${scmUrl}"
     
     // Importa la etapa desde la carpeta src de la misma librería
-    @Library('devops') _
+    //@Library('devops') _
 
     // Llama a la función de la etapa (especifica la ruta completa si es necesario)
     devops.bar(scmUrl)
