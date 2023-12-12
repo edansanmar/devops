a57743120849d10c3f9065443af1b9527f7284f4
edansanmar <esmartinezea2@gmail.com>
2023-12-12 13:51:12 -0500

    Prueba con el Pipeline completo

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,24 +1,61 @@
-// File: pipelineGeneral.groovy
-// File: pipelineGeneral.groovy
-def call(Map params) {
+//File: pipelineGeneral.groovvy
+def call (Map params) {
     def scmUrl = params.scmUrl
 
-    echo "Deploying backend with SCM URL: ${scmUrl}"
-    
-    // Pipeline, para probar el funcionamiento de la librería compartida
-    pipeline {
+    echo "Deploying backend wiht SCM URL: ${scmUrl}"
+pipeline {
         agent any
+
         stages {
-            
-            stage('Step 1') {
+            stage('Checkout') {
                 steps {
-                    // Llama a la función de la biblioteca compartida pasando scmUrl como argumento
-                    script {
-                        prueba.call(scmUrl)
+                    git url: scmUrl
+                }
+            }
+
+            stage('Build Application') {
+                steps {
+                    sh 'mvn clean package'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    sh 'mvn package'
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
+                    }
+                    success {
+                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
+                    }
+                }
+            }
+
+            stage('SonarQube analysis') {
+                environment {
+                    scannerHome = tool 'SonarqubeScanner'
+                }
+                steps {
+                    withSonarQubeEnv('ServerSonarqube') {
+                        sh "${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=analisisTermometro \
+                            -Dsonar.projectName=analisisTermometro \
+                            -Dsonar.sources=src/main/java \
+                            -Dsonar.java.binaries=target/classes \
+                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
                     }
                 }
             }
         }
     }
-}
+}  
+
 
