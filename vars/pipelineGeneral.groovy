58a7e7eabde6a937803edf4a5b9014e7fb78b20a
edansanmar <esmartinezea2@gmail.com>
2023-12-12 17:29:38 -0500

    CAMBIO DEL REPOSITORIO REMOTO

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,61 +1,22 @@
-//File: pipelineGeneral.groovvy
-def call (Map params) {
+// File: pipelineGeneral.groovy
+def call(Map params) {
     def scmUrl = params.scmUrl
 
-    echo "Deploying backend wiht SCM URL: ${scmUrl}"
-pipeline {
+    echo "Deploying backend with SCM URL: ${scmUrl}"
+    
+    // Pipeline, para probar el funcionamiento de la librería compartida
+    pipeline {
         agent any
-
         stages {
-            stage('Checkout') {
+            //Cambio a subir al repositorio remoto
+            stage('Step 1') {
                 steps {
-                    git url: scmUrl
-                }
-            }
-
-            stage('Build Application') {
-                steps {
-                    sh 'mvn clean package'
-                }
-            }
-
-            stage('Test') {
-                steps {
-                    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report' // Ejecuta las pruebas y genera el informe de cobertura con JaCoCo
-                }
-            }
-
-            stage('Package') {
-                steps {
-                    sh 'mvn package'
-                }
-                post {
-                    always {
-                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
-                    }
-                    success {
-                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
-                    }
-                }
-            }
-
-            stage('SonarQube analysis') {
-                environment {
-                    scannerHome = tool 'SonarqubeScanner'
-                }
-                steps {
-                    withSonarQubeEnv('ServerSonarqube') {
-                        sh "${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=analisisTermometro \
-                            -Dsonar.projectName=analisisTermometro \
-                            -Dsonar.sources=src/main/java \
-                            -Dsonar.java.binaries=target/classes \
-                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+                    // Llama a la función de la biblioteca compartida pasando scmUrl como argumento
+                    script {
+                        prueba.call(scmUrl)
                     }
                 }
             }
         }
     }
-}  
-
-
+}
