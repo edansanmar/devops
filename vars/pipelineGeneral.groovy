0f8f795cfdd5e1e3a6292e35d2b93a99583fa4e2
edansanmar <esmartinezea2@gmail.com>
2023-12-13 09:22:07 -0500

    "Nueva prueba de funcionamiento "

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,18 +1,11 @@
-// vars/pipelineGeneral.groovy
 def call(Map params) {
     def scmUrl = params.scmUrl
 
     echo "Deploying backend with SCM URL: ${scmUrl}"
-    pipeline{
-        agent any
-        stages{
-            stage('Clonar y verificar el repositorio'){
-                steps{
-                    script{
-                        clonarycapturar(scmUrl)
-                    }
-                }
-            }
-        }
-    }
+    
+    // Importa la función desde la carpeta src de la misma librería
+    @Library('devops.ClonarYCapturar') _
+    
+    // Llama a la función clonarycapturar pasando scmUrl como argumento
+    clonarycapturar(scmUrl)
 }
