3cc290c56410469d79cb436b1cf69268b22de1bb
edansanmar <esmartinezea2@gmail.com>
2023-12-13 13:12:29 -0500

    Cambio

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,16 +1,23 @@
  // Importa la función desde la carpeta src de la misma librería
-    @Library('devops') _
+@Library('devops') _
+
 def call(Map params) {
     def scmUrl = params.scmUrl
 
     echo "Deploying backend with SCM URL: ${scmUrl}"
-    pipeline{
-        stages{
-            stage('Verificar repo'){
-                def clonarInstancia = new etapa.reto.clonarMethod()
-                clonarInstancia(scmUrl)
+    
+    pipeline {
+        agent any
+        stages {
+            stage('Verificar repo') {
+                steps {
+                    // Crear una instancia de la clase y llamar a la función
+                    script {
+                        def clonarInstancia = new etapa.reto.clonarMethod()
+                        clonarInstancia.call(scmUrl)
+                    }
+                }
             }
         }
     }
-   
 }
