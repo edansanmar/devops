0291740240dacf917eeceb52b42fa4bdfb024ec6
edansanmar <esmartinezea2@gmail.com>
2023-12-13 09:23:36 -0500

    "Nuevo cambio"

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,10 +1,11 @@
+ // Importa la función desde la carpeta src de la misma librería
+    @Library('devops.ClonarYCapturar') _
 def call(Map params) {
     def scmUrl = params.scmUrl
 
     echo "Deploying backend with SCM URL: ${scmUrl}"
     
-    // Importa la función desde la carpeta src de la misma librería
-    @Library('devops.ClonarYCapturar') _
+   
     
     // Llama a la función clonarycapturar pasando scmUrl como argumento
     clonarycapturar(scmUrl)
