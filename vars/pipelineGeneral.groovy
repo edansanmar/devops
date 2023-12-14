21ef10393f2679c6c2f41b5c8450d65410bca885
edansanmar <esmartinezea2@gmail.com>
2023-12-14 12:07:28 -0500

    nuevo re cambio

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -10,7 +10,7 @@ pipeline {
         stages {
             stage('Checkout') {
                 steps {
-                    load 'src/etapas/reto/clonar.groovy'
+                
                    // git url: scmUrl
                     clonar()
                 }
