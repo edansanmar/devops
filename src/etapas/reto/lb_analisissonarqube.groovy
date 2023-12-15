1083cba31be8d55380385d9b68eac9514847350b
edansanmar <esmartinezea2@gmail.com>
2023-12-15 10:38:30 -0500

    Nuevo cambio para entender el funcionamiento de la etapa de Sonar

--- src/etapas/reto/lb_analisissonarqube.groovy
+++ src/etapas/reto/lb_analisissonarqube.groovy
@@ -30,16 +30,23 @@ def empaquetadoPackage() {
 }
 
 def sonarQube() {
-        def scannerHome = tool 'SonarqubeScanner'
-    withSonarQubeEnv('ServerSonarqube') {
-        sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=analisisTermometro \
-            -Dsonar.projectName=analisisTermometro \
-            -Dsonar.sources=src/main/java \
-            -Dsonar.java.binaries=target/classes \
-            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
-    }
+    script{
+        environment {
+            scannerHome = tool 'SonarqubeScanner'
+        }
+        steps {
+            withSonarQubeEnv('ServerSonarqube') {
+                sh "${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=analisisTermometro \
+                    -Dsonar.projectName=analisisTermometro \
+                    -Dsonar.sources=src/main/java \
+                    -Dsonar.java.binaries=target/classes \
+                    -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+            }
+        }
 
-    echo "Finalización de prueba en sonarQube"
+        echo "Finalización de prueba en sonarQube"
+
+    }
 }
 
