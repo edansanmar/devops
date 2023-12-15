1b6b0cf032a2535f4eec8ccb74c9453ad1142875
edansanmar <esmartinezea2@gmail.com>
2023-12-15 09:05:09 -0500

    correción de error

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -23,9 +23,9 @@ pipeline {
                         cleann.call()
                     }
                 }
-            }
+            }*/
 
-                  }*/
+                  }
     }
   
 }
