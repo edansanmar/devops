762c8244a7d4d281bb7d61b48d2865189f8c99a1
edansanmar <esmartinezea2@gmail.com>
2023-12-15 09:41:06 -0500

    cambio para probar el funcionameinto

--- src/etapas/reto/lb_analisissonarqube.groovy
+++ src/etapas/reto/lb_analisissonarqube.groovy
@@ -30,19 +30,20 @@ def empaquetadoPackage() {
 }
 
 def sonarQube() {
-          environment {
-                    scannerHome = tool 'SonarqubeScanner'
-                }
-                steps {
-                    withSonarQubeEnv('ServerSonarqube') {
-                        sh "${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=analisisTermometro \
-                            -Dsonar.projectName=analisisTermometro \
-                            -Dsonar.sources=src/main/java \
-                            -Dsonar.java.binaries=target/classes \
-                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
-                    }
-                }
+        environment {
+            scannerHome = tool 'SonarqubeScanner'
+        }
+        steps {
+            withSonarQubeEnv('ServerSonarqube') {
+                sh "${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=analisisTermometro \
+                    -Dsonar.projectName=analisisTermometro \
+                    -Dsonar.sources=src/main/java \
+                    -Dsonar.java.binaries=target/classes \
+                    -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+            }
+        }
+
         echo "Finalización de prueba en sonarQube"
 }
 
