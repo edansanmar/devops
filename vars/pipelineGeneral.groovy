2c7c620a66f9f4e0cea004e45318ca0d9fa7f649
edansanmar <esmartinezea2@gmail.com>
2023-12-18 11:02:09 -0500

    Prueba del funcionamiento del Pipeline,  con las dos etapas en el pipeline general

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,55 +1,63 @@
-def call (Map params) {
+def call(Map params) {
     def scmUrl = params.scmUrl
 
-    echo "Deploying backend wiht SCM URL: ${scmUrl}"
-pipeline {
+    echo "Deploying backend with SCM URL: ${scmUrl}"
+
+    pipeline {
         agent any
         stages {
             stage('Checkout') {
                 steps {
-                    script{
-                        def clonarr= new etapas.reto.lb_analisissonarqube()
+                    script {
+                        def clonarr = new etapas.reto.lb_analisissonarqube()
                         clonarr.clonarCheckout(scmUrl)
-                        
                     }
-                  
-                    
                 }
             }
-            stage('Build Applicarion') {
+            stage('Build Application') {
                 steps {
-                    script{
-                        def cleann= new etapas.reto.lb_analisissonarqube()
+                    script {
+                        def cleann = new etapas.reto.lb_analisissonarqube()
                         cleann.construirBuild()
                     }
                 }
             }
             stage('Test') {
                 steps {
-                    script{
-                        def pruebaa= new etapas.reto.lb_analisissonarqube()
+                    script {
+                        def pruebaa = new etapas.reto.lb_analisissonarqube()
                         pruebaa.pruebaTest()
                     }
                 }
             }
-            
-                node('Package') {
-                    script{
-                        def empaquetadoo= new etapas.reto.lb_analisissonarqube()
-                        empaquetadoo.empaquetadoPackage()
+            stage('Package') {
+                steps {
+                    sh 'mvn package'
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
+                    }
+                    success {
+                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
                     }
                 }
-            
-            /*stage('SonarQube analysis') {
-                node {
-                    script{
-                        def analisiss= new etapas.reto.lb_analisissonarqube()
-                        analisiss.sonarQube()
+            }
+            stage('SonarQube analysis') {
+                environment {
+                    scannerHome = tool 'SonarqubeScanner'
+                }
+                steps {
+                    withSonarQubeEnv('ServerSonarqube') {
+                        sh "${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=analisisTermometro \
+                            -Dsonar.projectName=analisisTermometro \
+                            -Dsonar.sources=src/main/java \
+                            -Dsonar.java.binaries=target/classes \
+                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
                     }
                 }
-            }*/
-
-                  }
+            }
+        }
     }
-  
 }
