43c567311917b1c33d77a46bed266f77ba5ebb10
edansanmar <esmartinezea2@gmail.com>
2023-12-18 10:56:54 -0500

    arreglo para el funcionamiento del código

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -47,9 +47,9 @@ pipeline {
                         analisiss.sonarQube()
                     }
                 }
-            }
+            }*/
 
-                  }*/
+                  }
     }
   
 }
