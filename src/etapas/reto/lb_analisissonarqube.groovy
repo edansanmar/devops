ac7064474b653935541f0d37c938da971dd51a4d
edansanmar <esmartinezea2@gmail.com>
2023-12-18 09:56:09 -0500

    Nueva prueba para el funcionamiento del sonar

--- src/etapas/reto/lb_analisissonarqube.groovy
+++ src/etapas/reto/lb_analisissonarqube.groovy
@@ -34,17 +34,15 @@ def empaquetadoPackage() {
 
 def sonarQube() {
         def scannerHome = tool 'SonarqubeScanner'
-    withSonarQubeEnv('ServerSonarqube') {
-        sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=analisisTermometro \
-            -Dsonar.projectName=analisisTermometro \
-            -Dsonar.sources=. \
-            -Dsonar.java.binaries=.\
-            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+withSonarQubeEnv('ServerSonarqube') {
+    sh "${scannerHome}/bin/sonar-scanner \
+        -Dsonar.projectKey=analisisTermometro \
+        -Dsonar.projectName=analisisTermometro \
+        -Dsonar.sources=. \
+        -Dsonar.java.binaries=. \
+        -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+}
 
-             // Asignar la ruta del informe Jacoco
-        //def jacocoReportPath = "target/site/jacoco/jacoco.xml"
-    }
 
     echo "Finalización de prueba en sonarQube"
     //return jacocoReportPath
