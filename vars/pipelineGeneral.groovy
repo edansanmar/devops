e1c9721c3136e5af9a316b20b8145bf03d8f0a09
edansanmar <esmartinezea2@gmail.com>
2023-12-18 14:47:38 -0500

    Prueba de funcionamiento

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,3 +1,4 @@
+//File: pipelineGeneral.groovvy
 def call(Map params) {
     def scmUrl = params.scmUrl
 
@@ -30,7 +31,34 @@ def call(Map params) {
                     }
                 }
             }
-            
+            stage('Package') {
+                steps {
+                    sh 'mvn package'
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/TEST-*.xml' // Patrón para los archivos XML de pruebas
+                    }
+                    success {
+                        archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false // Archivar el archivo JAR generado
+                    }
+                }
+            }
+            stage('SonarQube analysis') {
+                environment {
+                    scannerHome = tool 'SonarqubeScanner'
+                }
+                steps {
+                    withSonarQubeEnv('ServerSonarqube') {
+                        sh "${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=analisisTermometro \
+                            -Dsonar.projectName=analisisTermometro \
+                            -Dsonar.sources=src/main/java \
+                            -Dsonar.java.binaries=target/classes \
+                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+                    }
+                }
+            }
         }
     }
-    
+}
