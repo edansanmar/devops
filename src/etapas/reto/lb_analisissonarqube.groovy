479a9c55e4d0bd8b79b568c9d59bbbdc9af09f8a
edansanmar <esmartinezea2@gmail.com>
2023-12-19 15:43:44 -0500

    reintento de funcionamiento del informe de sonarQube

--- src/etapas/reto/lb_analisissonarqube.groovy
+++ src/etapas/reto/lb_analisissonarqube.groovy
@@ -5,8 +5,8 @@ withSonarQubeEnv('ServerSonarqube') {
     sh "${scannerHome}/bin/sonar-scanner \
         -Dsonar.projectKey=analisisTermometro \
         -Dsonar.projectName=analisisTermometro \
-        -Dsonar.sources=src/main/java \
-        -Dsonar.java.binaries=target/classes \
+        -Dsonar.sources=. \
+        -Dsonar.java.binaries=. \
         -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
 }
 
