77c777acb92be7e1a5c91f4f39ae39949ab1b52d
edansanmar <esmartinezea2@gmail.com>
2023-12-20 17:43:41 -0500

    Uso de la forma con jacoco

--- src/etapas/reto/lb_buildartefacto.groovy
+++ src/etapas/reto/lb_buildartefacto.groovy
@@ -13,9 +13,9 @@ def construirBuild() {
 
 def pruebaTest() {
         sh 'mvn clean package'
-        sh 'mvn test'
+        //sh 'mvn test'
         //sh 'mvn clean verify' //Generación del informe de cobertura
-        //sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report'
+        sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test jacoco:report'
         echo "Finalización de Build Applicarion"
 }
 
