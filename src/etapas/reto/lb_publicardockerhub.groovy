7ae8b43da9e54fffa3cf1ce7822c75836f22a273
edansanmar <esmartinezea2@gmail.com>
2024-01-03 17:32:59 -0500

    Arreglo realizado en la función de construcción de la imagen, así como en la de publicación de la imagen

--- src/etapas/reto/lb_publicardockerhub.groovy
+++ src/etapas/reto/lb_publicardockerhub.groovy
@@ -2,18 +2,18 @@
 package etapas.reto
 def publicarimagen(){
     def dockerHubUsername = 'edansama96'
-    def dockerHubTokenCredentialId= 'tokendockerhub'
-    def imageExists = sh(script: "docker images -q ${dockerHubUsername}/termometro-buildimagen"
-    ,returnStdout: true).trim()
-    if(imageExists){
-        echo "La imagen ${dockerHubUsername}/termometro-buildimagen ya existe en Docker Hub. Descartndo Publicación... "
+    def dockerHubTokenCredentialId = 'tokendockerhub'
+    def imageExists = sh(script: "docker images -q ${dockerHubUsername}/termometro-buildimagen",
+    returnStdout: true).trim()
+    if (imageExists) {
+        echo "La imagen ${dockerHubUsername}/termometro-buildimagen ya existe en Docker Hub. Descartando publicación..."
     } else {
         withCredentials([usernamePassword(credentialsId: dockerHubTokenCredentialId, passwordVariable:
         'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]){
-            sh "docker login --username ${env.DOCKERHUB_USERNAME} --password ${env.DOCKERHUB_PASSWORD}"
+          sh "docker login --username $(env.DOCKERHUB_USERNAME) --password $(env.DOCKERHUB_PASSWORD)"  
         }
-        sh "docker tag termometro-buildimagen ${dockerHubUsername} "
-        sh "docker push ${dockerHubUsername}/termometro-buildimagen"
+        sh "docker tag termometro-buildimagen ${dockerHubUsername}/termometro-buildimagen"
+        sh "docker push ${dockerHubUsername}/termometro-buildimagen:latest"
     }
 }
 
