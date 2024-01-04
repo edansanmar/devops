36d58492b38d56b03d983c7a0d72b4ade89eccd4
edansanmar <esmartinezea2@gmail.com>
2024-01-04 16:13:01 -0500

    Nuevo cambio realizado, para realizar un pull request

--- src/etapas/reto/lb_buildimagen.groovy
+++ src/etapas/reto/lb_buildimagen.groovy
@@ -2,8 +2,7 @@ package etapas.reto
 //Se procedera a crear una función, para la implementación de una imagen del proyecto
 def createImage(){
    //Se establece una variable, la cual se utiliza para 
- def imageExists = sh(script: "docker images -q termometro-buildimagen", 
- returnStdout: true).trim()
+ def imageExists = sh(script: "docker images -q termometro-buildimagen", returnStdout: true).trim()
  if(imageExists){
     echo "La imagen termometro-buildimagen ya existe. Descartando construcción..."
  }else {
