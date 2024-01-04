ed28ab2e245cee9a213ce54962db3478f27010f2
edansanmar <esmartinezea2@gmail.com>
2024-01-04 16:06:52 -0500

    Arreglo  del uso del ciclo if else, en la etapa de publicación

--- src/etapas/reto/lb_publicardockerhub.groovy
+++ src/etapas/reto/lb_publicardockerhub.groovy
@@ -6,11 +6,11 @@ def publicarimagen() {
     def dockerHubTokenCredentialId = 'tokendockerhub'
 
     // Verificar si la imagen ya existe localmente
-    def imageExists = sh(script: "docker images -q ${dockerHubUsername}/termometro-buildimagen", returnStdout: true).trim()
+    def imageExists = sh(script: "docker images -q ${env.DOCKERHUB_USERNAME}/termometro-buildimagen", returnStdout: true).trim()
 
-   /* if (imageExists) {
+    if (imageExists) {
         // Si la imagen ya existe, imprimir un mensaje y descartar la publicación
-        echo "La imagen ${dockerHubUsername}/termometro-buildimagen ya existe en Docker Hub. Descartando publicación..."
+        echo "La imagen ${env.DOCKERHUB_USERNAME}/termometro-buildimagen ya existe en Docker Hub. Descartando publicación..."
     } else {
         // Autenticación en Docker Hub usando credenciales de Jenkins
         withCredentials([usernamePassword(credentialsId: dockerHubTokenCredentialId,
@@ -20,14 +20,14 @@ def publicarimagen() {
         }
 
         // Etiquetar la imagen local con el nombre de Docker Hub y luego empujarla a Docker Hub
-       // sh "docker tag termometro-buildimagen ${dockerHubUsername}/termometro-buildimagen"
-      //  sh "docker push ${dockerHubUsername}/termometro-buildimagen:latest"
+        sh "docker tag termometro-buildimagen ${env.DOCKERHUB_USERNAME}/termometro-buildimagen"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/termometro-buildimagen"
        
-    }*/
+    }
     //sh "docker push ${dockerHubUsername}/termometro-buildimagen"
     
      // Autenticación en Docker Hub utilizando las credenciales almacenadas en Jenkins
-                    withCredentials([usernamePassword(credentialsId: 'dockerHubTokenCredentialId',
+                   /* withCredentials([usernamePassword(credentialsId: 'dockerHubTokenCredentialId',
                                                      passwordVariable: 'DOCKERHUB_PASSWORD',
                                                      usernameVariable: 'DOCKERHUB_USERNAME')]) {
                         sh "docker login --username ${env.DOCKERHUB_USERNAME} --password ${env.DOCKERHUB_PASSWORD}"
@@ -37,7 +37,7 @@ def publicarimagen() {
                         //sh 'docker push miusuario/mi-imagen:latest'
                         sh 'docker push ${env.DOCKERHUB_USERNAME}/termometro-buildimagen'
 
-                                                     }
+                                                     }*/
 
 }
 
